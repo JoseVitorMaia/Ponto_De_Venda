61bbba576c7edfd4ec34c3c95fa6adf8ad15007b
José Vitor Maia <josevitor.wow@hotmail.com>
2023-06-23 14:19:22 -0300

    Pdv 2.0
    Correção exclusão de itens no carrinho

--- script.js
+++ script.js
@@ -1,167 +1,127 @@
-const inventario = {
-    "001":{
-        "nome": "Teclado Logitech",
-        "valor": "120.00"
+const produtos = [
+    {
+        id:0,
+        desc: "Mouse",
+        valor: 30
+    },
+    {
+        id:1,
+        desc: "Teclado",
+        valor: 50
     },
 
-    "002":{
-        "nome": "Mouse Logitech",
-        "valor": "80.00"
+    {
+        id:2,
+        desc: "Monitor",
+        valor: 1200
     },
 
-    "003":{
-        "nome": "Mousepad Warrior",
-        "valor": "40.00"
+    {
+        id:3,
+        desc: "Gabinete",
+        valor: 300
     },
 
-    "004":{
-        "nome": "Monitor Lg Ultragear 27''",
-        "valor": "1200.00"
+    {
+        id:4,
+        desc: "Headset",
+        valor: 450
     },
 
-    "005":{
-        "nome": "Caixa de Som",
-        "valor": "40.00"
+    {
+        id:5,
+        desc: "Cadeira Gamer",
+        valor: 800
     },
 
-    "006":{
-        "nome": "Headset Corsair Raptor HS30",
-        "valor": "220.00"
+    {
+        id:6,
+        desc: "Xbox Series X",
+        valor: 4300
     },
 
-    "007":{
-        "nome": "Gabinete Warrior",
-        "valor": "180.00"
+    {
+        id:7,
+        desc: "Playstation 5",
+        valor: 4200
     }
-}
+]
 
 
-/*
-function mostrarVenda() {
-    let mostrarVenda = document.querySelector('#venda')
-    if (mostrarVenda.style.display == "none") {
-        mostrarVenda.style.display = "block"
-    } else {
-        mostrarVenda.style.display = "none"
+const itensVenda = document.querySelector('#itensVenda')
+const addCarrinho = document.querySelector('#addCarrinho')
+const carrinho = document.querySelector('#carrinho')
+const totalCompra = document.querySelector('#totalCompra')
+const finalizarCompra = document.querySelector('#finalizarCompra')
+
+let total = 0
+
+
+
+
+window.addEventListener("DOMContentLoaded",function(){
+    for (i = 0; i < produtos.length; i++){
+        const item = document.createElement('option')
+        item.innerText = produtos[i].desc + " R$ " + produtos[i].valor
+        item.value = produtos[i].valor
+
+        itensVenda.appendChild(item)
     }
-}
-
-
-function mostrarProdutos() {
-    let listaProdutos = document.querySelector('#listaProdutos')
-    if (listaProdutos.style.display == "none") {
-        listaProdutos.style.display = "block"
-    } else {
-        listaProdutos.style.display = "none"
-    }
-}
-
-*/
-
-function selecionarProduto() {
-    let produtos = document.querySelector('#produtos')
-    console.log(`Escolheu o item ${inventario[produtos.value].nome} com o valor de ${inventario[produtos.value].valor} `)
-}
-
-
-let soma = 0;
-let id = 0;
-let carrinho = [];
-let resumo = [];
-
-let data = new Date()
-let hora = data.getHours()
-let minutos = data.getMinutes()
-let dia = String(data.getDate()).padStart(2 ,'0')
-let mes = String(data.getMonth() + 1).padStart(2,'0')
-let ano = data.getFullYear()
-let dataAtual = `${dia}/${mes}/${ano} às ${hora}:${minutos}`
-
-
-function addCarrinho() {
-    //Mostrar itens que estão sendo comprando
-    let compra = document.querySelector('#compra')
-    let produtos = document.querySelector('#produtos')
-
-    let item = document.createElement('option')
-    item.setAttribute("value",id)
-    item.setAttribute("ondblclick","deletarItem(this.value)")
-    item.innerHTML = (`- Item : ${inventario[produtos.value].nome} - Valor: R$${inventario[produtos.value].valor} `)
-
     
-    let itemValor = document.createElement('option')
-    itemValor.setAttribute("value",inventario[produtos.value].valor)
-    itemValor.setAttribute("ondblclick","deletarItem(this.value)")
-    itemValor.innerHTML = inventario[produtos.value].valor
-
-    resumo.push(itemValor)
-
-    carrinho.push(item)
-    compra.appendChild(carrinho[id])
+})
 
 
-    id++;
+addCarrinho.addEventListener('click',function(){
+    itemCarrinho = document.createElement('option')
+    itemCarrinho.value = itensVenda.options[itensVenda.selectedIndex].value
+    itemCarrinho.setAttribute("class", "item");
+    itemCarrinho.setAttribute("ondblclick","excluir()")
+    itemCarrinho.innerText = itensVenda.options[itensVenda.selectedIndex].text
+
+    carrinho.appendChild(itemCarrinho)
+
+    somaCompra()
+})
 
 
-    //Atualizar valor total no rodapé
-    valorDaCompra()
 
+function somaCompra() { 
+    total += Number(itemCarrinho.value)
+    totalCompra.innerText = `Total: R$ ${total}`
 }
 
 
-function valorDaCompra() {
-    //Mostrar valor total no rodapé
-    let totalCompra = document.querySelector('.totalCompra')
-    let total = 0
-
-    for (i = 0; i < resumo.length; i++){
-        total += Number(resumo[i].value)
-    }
-
-    totalCompra.innerText = (`Resumo: R$` + total)
-    totalRecibo.innerText = (`Total: R$` + total)
-
-    soma = total
-}
-
-function deletarItem(x) {
-    carrinho[x].remove()
-    resumo.splice(0,1)
-    
-    //Atualizar valor total no rodapé
-    valorDaCompra()
+function excluir() {
+    const e = carrinho.selectedIndex
+    total -= Number(carrinho[e].value)
+    totalCompra.innerText = `Total: R$ ${total}`
+    carrinho[e].remove()
     
 }
 
+finalizarCompra.addEventListener('click',function(){
+    let janelaCarrinho = document.querySelector('#janelaCarrinho')
+    let rodape = document.querySelector('#rodape')
 
-function gerarRecibo() {
-    let telaFinalizarCompra = document.querySelector('#telaFinalizarCompra')
-    let telaReciboCompra = document.querySelector('#telaReciboCompra')
-    telaFinalizarCompra.style.display = "block"
-    venda.style.display = 'none'
-    //containerLateral.style.display = 'none'
-    console.log(resumo)
-    console.log(carrinho)
-
-    for (i = 0; i < compra.length; i++) {
-        telaReciboCompra.innerHTML += compra[i].innerHTML + "\n"
-    }
-
-    
-}
-
-function voltarCarrinho() {
-    telaFinalizarCompra.style.display = "none"
-    venda.style.display = 'block'
-    //containerLateral.style.display = 'grid'
-    telaReciboCompra.innerHTML = ''
-}
-
-function finalizarRecibo() {
-    let telaReciboCompra = document.querySelector('#telaReciboCompra')
 
     var doc = new jsPDF()
-    doc.text("Obrigado(a) pela preferência !!\n" + "--------------------------------------\n" + "Resumo da sua compra: \n" + telaReciboCompra.innerHTML + "--------------------------------------\n" + `Total a pagar: R$` + soma + `\nCompra realizada ${dataAtual}`,20,30)
+    doc.text(`Obrigado pela preferência! \n - - - - - - - - - - - - - - - - - - -\nSegue a lista com seus itens:\n \n${carrinho.innerText}\n - - - - - - - - - - - - - - - - - - -\n Total da sua compra: R$${total}`,20,30)
+
+    let modal = document.createElement('div')
+    modal.setAttribute('class','modal')
+
+    let btn_confirm = document.createElement('button')
+    btn_confirm.innerText = "Obrigado(a) pela compra!"
+    btn_confirm.setAttribute("class","btn_confirm")
+    btn_confirm.setAttribute("onclick","finalizarRefresh()")
+    modal.appendChild(btn_confirm)
+
+    janelaCarrinho.appendChild(modal)
+    rodape.remove()
     doc.save('Compra.pdf')
-    console.log(telaReciboCompra.innerHTML)
+})
+
+
+function finalizarRefresh() {
+    window.location.reload(true);
 }
